8f70fb7d75809a88a2f26ed610920b328927b926
administrator@STFCU.NET <administrator@STFCU.NET>
2022-07-18 11:27:13 -0500

    latestproduction

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ Description: Follow UP Documentation
 """
 from app import app
 from app import routes
-
+#Update TEst Allowed
 
 if __name__ == "__main__":
     # app.run(debug=True, host="0.0.0.0")
